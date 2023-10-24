6c68e850126875ae89e43012ba940c038698bdb4
Joelnicolass <joelnicolass@gmail.com>
2023-10-24 19:47:19 -0300

    test

--- vercel.sh
+++ vercel.sh
@@ -2,21 +2,13 @@
 
 source .env
 
-if [[ $VERCEL_GIT_COMMIT_REF == "PROD"  ]] ; then 
+if [[ $BRANCH_NAME == "PROD"  ]] ; then 
     npm run build:prod
-elif [[ $VERCEL_GIT_COMMIT_REF == "UAT"  ]] ; then
+elif [[ $BRANCH_NAME == "UAT"  ]] ; then
     npm run build:uat
-elif [[ $VERCEL_GIT_COMMIT_REF == "QA"  ]] ; then
+elif [[ $BRANCH_NAME == "QA"  ]] ; then
     npm run build:qa
 else
-    
-    echo $VITE_VERCEL_BRANCH_URL
-    echo $VERCEL_BRANCH_URL
-    echo $VITE_VERCEL_GIT_COMMIT_REF
-    echo $VERCEL_GIT_COMMIT_REF
-    echo $VERCEL_GIT_COMMIT_REF_TYPE
-    echo $VITE_VERCEL_GIT_COMMIT_REF_TYPE
     echo $BRANCH_NAME
-
     echo "BRANCH NOT MATCHED"
 fi
