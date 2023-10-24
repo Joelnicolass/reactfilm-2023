15df416caf128856e5a3c9c4f479e0c03a564f0c
Joelnicolass <joelnicolass@gmail.com>
2023-10-24 16:39:25 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <div>
       <h1>APP PARA PELIS</h1>
-      <h2>TEST</h2>
+      <h2>ESTO FALLA</h2>
     </div>
   );
 };
--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -6,6 +6,11 @@ import { describe, expect, it } from "vitest";
 describe("App", () => {
   it("should render component", () => {
     const component = render(<App />);
-    expect(component).toBeDefined();
+
+    const title = component.getByText("ESTO NO ESTA");
+
+    expect(title).toBe("asdasdasd");
+
+    //expect(component).toBeDefined();
   });
 });
