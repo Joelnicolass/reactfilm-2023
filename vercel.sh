e354ee38d14565075c5be5a04b86e7a79e4469b1
Joelnicolass <joelnicolass@gmail.com>
2023-10-24 18:27:19 -0300

    [cfg]- add environment per branch config. sh file

---
+++ vercel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ 
+if [[ $VERCEL_GIT_COMMIT_REF == "PROD"  ]] ; then 
+  npm run build:prod
+elif [[ $VERCEL_GIT_COMMIT_REF == "UAT"  ]] ; then
+    npm run build:uat
+elif [[ $VERCEL_GIT_COMMIT_REF == "QA"  ]] ; then
+    npm run build:qa
+else
+  echo "BRANCH NOT MATCHED"
+fi
