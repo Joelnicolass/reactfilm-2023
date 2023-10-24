ce6f1327f4cecc9fe05d79a1a8399b817a680bda
Joelnicolass <joelnicolass@gmail.com>
2023-10-24 16:42:05 -0300

    fix

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -6,11 +6,6 @@ import { describe, expect, it } from "vitest";
 describe("App", () => {
   it("should render component", () => {
     const component = render(<App />);
-
-    const title = component.getByText("ESTO NO ESTA");
-
-    expect(title).toBe("asdasdasd");
-
-    //expect(component).toBeDefined();
+    expect(component).toBeDefined();
   });
 });
