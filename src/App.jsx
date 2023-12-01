1683d188724cc460b2085378703cb3af94256d5b
Joelnicolass <joelnicolass@gmail.com>
2023-12-01 08:52:45 -0300

    test env

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { environment } from "@environments";
 const App = () => {
   return (
     <div>
+      {import.meta.ENV.VITE_APP_TMDB_API_KEY}
       <h1>APP PARA PELIS</h1>
       <h2>{environment.title}</h2>
     </div>
